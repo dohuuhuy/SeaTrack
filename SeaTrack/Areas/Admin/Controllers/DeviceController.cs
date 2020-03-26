22a150814d4b336dd327c92ed14370f6e9401463
huy <54833364+dohuuhuy@users.noreply.github.com>
2020-03-26 08:27:32 +0700

    controll decvice

---
+++ SeaTrack/Areas/Admin/Controllers/DeviceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SeaTrack.Lib.DTO;
+using SeaTrack.Models;
+
+namespace SeaTrack.Areas.Admin.Controllers
+{
+    public class DeviceController : Controller
+    {
+        // GET: Admin/Device
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ()
+        {
+            return View();
+        }
+        [HttpGet]
+        public HttpResponseMessage listdevice(int )
+        {
+            var rs = AdminService.GetListUser(RoleID);
+            return Request.CreateResponse(HttpStatusCode.OK, new
+            {
+                STATUSCODE = rs != null ? Util.Static.SUCCESS_CODE : Util.Static.ERROR_CODE,
+                DATA = rs
+            });
+        }
+    }
+}
