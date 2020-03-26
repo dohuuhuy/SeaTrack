7d6a1377be943d4e9e907e792eaa229a1c684cb5
mrhuy3200 <mrhuy3200113@gmail.com>
2020-03-26 08:26:32 +0700

    Thêm User Service

--- SeaTrack.Lib/Service/AdminService.cs
+++ SeaTrack.Lib/Service/AdminService.cs
@@ -49,6 +49,74 @@ namespace SeaTrack.Lib.Service
             }
             return lst;
         }
+
+        public static UserInfoDTO GetUserByID(int UserID)
+        {
+            UserInfoDTO user = null;
+            var reader = SqlHelper.ExecuteReader(ConnectData.ConnectionString, "sp_GetUserByID", UserID);
+            if (reader.HasRows)
+            {
+                user = new UserInfoDTO();
+                while (reader.Read())
+                {
+                    var data = new UserInfoDTO()
+                    {
+                        UserID = Convert.ToInt16(reader["UserID"]),
+                        Username = reader["Username"].ToString(),
+                        Password = reader["Password"].ToString(),
+                        Fullname = reader["FullName"].ToString(),
+                        Phone = reader["Phone"].ToString(),
+                        Address = reader["Address"].ToString(),
+                        Status = Convert.ToInt16(reader["Status"]),
+                        CreateBy = reader["CreateBy"].ToString(),
+                        CreateDate = Convert.ToDateTime(reader["CreateDate"]),
+                        UpdateBy = reader["UpdateBy"].ToString(),
+                        LastUpdateDate = Convert.ToDateTime(reader["LastUpdateDate"]),
+                        RoleID = Convert.ToInt16(reader["RoleID"]),
+                    };
+                    user = data;
+                }
+                return user;
+
+            }
+            return null;
+        }
+
+        public static bool EditUser(UserInfoDTO user)
+        {
+            try
+            {
+                int res = SqlHelper.ExecuteNonQuery(ConnectData.ConnectionString, "sp_EditUser", user.UserID, user.Username, user.Password, user.Fullname, user.Phone, user.Address, user.Status, user.UpdateBy, user.LastUpdateDate, user.RoleID);
+                if (res == 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteUser(int UserID)
+        {
+            try
+            {
+                int res = SqlHelper.ExecuteNonQuery(ConnectData.ConnectionString, "sp_DeleteUser", UserID);
+                if (res == 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
         #region
         public static int CreateDevice(Device device)
         {
